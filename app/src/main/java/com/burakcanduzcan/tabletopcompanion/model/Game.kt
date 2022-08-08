3e993d1459dd701cf2c2bc308950291aae65c873
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-08-08 22:45:22 +0300

    simplified SetupFragment.kt

--- app/src/main/java/com/burakcanduzcan/tabletopcompanion/model/Game.kt
+++ app/src/main/java/com/burakcanduzcan/tabletopcompanion/model/Game.kt
@@ -11,8 +11,7 @@ enum class Game(
     val maxPlayer: Int,
 ) {
     SCRABBLE(R.string.scrabble, R.drawable.scrabble_letter, 2, 4),
-    CHESS(R.string.chess, R.drawable.knight, 2, 2),
-    TBA(R.string.tba, R.drawable.ic_launcher_background, 0, 0);
+    CHESS(R.string.chess, R.drawable.knight, 2, 2);
 
     companion object {
         fun getList(): List<Game> = values().toList()
--- app/src/main/java/com/burakcanduzcan/tabletopcompanion/ui/setup/SetupFragment.kt
+++ app/src/main/java/com/burakcanduzcan/tabletopcompanion/ui/setup/SetupFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -14,17 +12,13 @@ import com.burakcanduzcan.tabletopcompanion.R
 import com.burakcanduzcan.tabletopcompanion.databinding.FragmentSetupBinding
 import com.burakcanduzcan.tabletopcompanion.model.Game
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
-class SetupFragment : Fragment(), AdapterView.OnItemSelectedListener {
+class SetupFragment : Fragment() {
 
     private lateinit var binding: FragmentSetupBinding
     private val args: SetupFragmentArgs by navArgs()
     private lateinit var selectedGame: Game
-
-    private var option1PlayerCount = 0
-    private var option2TimeSpan = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,109 +38,35 @@ class SetupFragment : Fragment(), AdapterView.OnItemSelectedListener {
     }
 
     private fun setupViews() {
-        //Options1: Player Count
-        if (enableCheck(requireContext().getString(selectedGame.nameRes), 1)) {
-            binding.cvOption1.visibility = View.VISIBLE
-            option1PlayerCount = selectedGame.minPlayer
-            binding.tvOption1PlayerCount.text = option1PlayerCount.toString()
+        //Option1 - Player Count
+        var playerCount: Int = selectedGame.minPlayer
+        binding.tvOption1PlayerCount.text = playerCount.toString()
 
-            binding.ibOption1Increase.setOnClickListener {
-                if (option1PlayerCount < selectedGame.maxPlayer) {
-                    option1PlayerCount++
-                    binding.tvOption1PlayerCount.text = option1PlayerCount.toString()
-                }
+        binding.ibOption1Increase.setOnClickListener {
+            if (playerCount < selectedGame.maxPlayer) {
+                playerCount++
+                binding.tvOption1PlayerCount.text = playerCount.toString()
             }
-            binding.ibOption1Decrease.setOnClickListener {
-                if (option1PlayerCount > selectedGame.minPlayer) {
-                    option1PlayerCount--
-                    binding.tvOption1PlayerCount.text = option1PlayerCount.toString()
-                }
-            }
-        } else {
-            binding.cvOption1.visibility = View.GONE
         }
-
-        //Options2: Timer
-        if (enableCheck(requireContext().getString(selectedGame.nameRes), 2)) {
-            binding.cvOption2.visibility = View.VISIBLE
-            option2TimeSpan = ""
-
-            //spinner setup
-            ArrayAdapter.createFromResource(
-                requireContext(),
-                R.array.time_spans,
-                android.R.layout.simple_spinner_item
-            ).also { arrayAdapter ->
-                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                binding.spOption2.adapter = arrayAdapter
+        binding.ibOption1Decrease.setOnClickListener {
+            if (playerCount > selectedGame.minPlayer) {
+                playerCount--
+                binding.tvOption1PlayerCount.text = playerCount.toString()
             }
-            binding.spOption2.onItemSelectedListener = this
-
-            binding.cbOption2Timer.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    binding.spOption2.visibility = View.VISIBLE
-                } else {
-                    binding.spOption2.visibility = View.INVISIBLE
-                }
-            }
-
-        } else {
-            binding.cvOption2.visibility = View.GONE
         }
 
         //finish setup button
         binding.btnNext.setOnClickListener {
-            //this will proceed to next screen
-
-            //check if a timer is given
-            if (binding.cbOption2Timer.isChecked) {
+            if (binding.etOption2Duration.text.toString().isBlank()) {
+                binding.etOption2Duration.error =
+                    requireContext().getString(R.string.duration_field_can_not_be_empty)
+            } else {
                 this.findNavController().navigate(SetupFragmentDirections.finishSetup(
                     selectedGame,
-                    option1PlayerCount,
-                    option2TimeSpan
+                    binding.tvOption1PlayerCount.text.toString().toInt(),
+                    binding.etOption2Duration.text.toString()
                 ))
-            } else {
-                this.findNavController()
-                    .navigate(SetupFragmentDirections.finishSetup(
-                        selectedGame,
-                        option1PlayerCount
-                    ))
             }
         }
-    }
-
-    private fun enableCheck(gameName: String, option: Int): Boolean {
-        when (option) {
-            1 -> {
-                if (gameName == getString(R.string.scrabble)) {
-                    //show option1 for scrabble
-                    return true
-                } else if (gameName == getString(R.string.chess)) {
-                    //show option1 for chess
-                    return true
-                }
-            }
-            2 -> {
-                if (gameName == getString(R.string.scrabble)) {
-                    //show option2 for scrabble
-                    return true
-                } else if (gameName == getString(R.string.chess)) {
-                    //show option2 for chess
-                    return true
-                }
-            }
-            else -> {
-                return false
-            }
-        }
-        return false
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-        Timber.i(parent!!.getItemAtPosition(pos).toString())
-        option2TimeSpan = parent.getItemAtPosition(pos).toString()
-    }
-
-    override fun onNothingSelected(p0: AdapterView<*>?) {
     }
 }
