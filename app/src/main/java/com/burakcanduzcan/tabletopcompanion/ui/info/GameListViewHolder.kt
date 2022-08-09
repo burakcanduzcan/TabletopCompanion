4dead48b123007b2548994a61f89e19f4f64a8b3
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-08-09 17:11:07 +0300

    limited timer input to numeric characters, updated game list's player count description logic

--- app/src/main/java/com/burakcanduzcan/tabletopcompanion/ui/info/GameListViewHolder.kt
+++ app/src/main/java/com/burakcanduzcan/tabletopcompanion/ui/info/GameListViewHolder.kt
@@ -24,25 +24,22 @@ class GameListViewHolder(
         binding.tvTitle.setText(game.nameRes)
 
         //description
-        if (game.minPlayer != game.maxPlayer) {
-            //if player count is a range: M-N Players
-            binding.tvDescription.text =
-                "${game.minPlayer}-${game.maxPlayer} ${context.getString(R.string.Players)}"
-        } else if (game.minPlayer == 0) {
-            // if player count isn't given: TBA
-            //un-clickable
+        if (game.minPlayer == 0) {
+            // if player count isn't given: TBA and un-clickable
             binding.tvDescription.text = context.getString(R.string.tba)
             binding.cardView.isEnabled = false
+        } else if (game.minPlayer == 1) {
+            //if it's a single player game: M Player
+            binding.tvDescription.text =
+                context.getString(R.string.numbered_min_player, game.minPlayer)
+        } else if (game.minPlayer != game.maxPlayer) {
+            //if player count is a range: M-N Players
+            binding.tvDescription.text =
+                context.getString(R.string.numbered_min_max_players, game.minPlayer, game.maxPlayer)
         } else {
-            if (game.minPlayer == 1) {
-                //if it's a single player game: M Player
-                binding.tvDescription.text =
-                    "${game.minPlayer} ${context.getString(R.string.Player)}"
-            } else {
-                //if it has fixed number of players but not one: N Players
-                binding.tvDescription.text =
-                    "${game.minPlayer} ${context.getString(R.string.Players)}"
-            }
+            //if it has fixed number of players but not one: N Players
+            binding.tvDescription.text =
+                context.getString(R.string.numbered_min_players, game.minPlayer)
         }
     }
 }
