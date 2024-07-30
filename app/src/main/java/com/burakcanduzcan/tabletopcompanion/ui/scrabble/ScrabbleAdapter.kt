d401647a2f93c04a214105735e400c278507e504
burakcanduzcan <burakcanduzcan@gmail.com>
2024-07-30 16:14:37 +0300

    Improve code

--- app/src/main/java/com/burakcanduzcan/tabletopcompanion/ui/scrabble/ScrabbleAdapter.kt
+++ app/src/main/java/com/burakcanduzcan/tabletopcompanion/ui/scrabble/ScrabbleAdapter.kt
@@ -32,69 +32,75 @@ class ScrabbleAdapter(
     inner class ScrabbleViewHolder(
         private val binding: ItemScrabblePlayerBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
-
         fun bind(scrabblePlayer: ScrabblePlayer) {
-            //player name
-            binding.tvPlayerName.text = scrabblePlayer.playerName
+            with(binding) {
+                //player name
+                tvPlayerName.text = scrabblePlayer.playerName
 
-            //edit player name button
-            binding.ibChangeName.setOnClickListener {
-                //create and show the dialog
-                val dialogBinding = PopupPlayerNameBinding.inflate(layoutInflater)
-                AlertDialog.Builder(context)
-                    .setView(dialogBinding.root)
-                    .setTitle(context.getString(R.string.change_player_name))
-                    .setPositiveButton(context.getString(R.string.change)) { _, _ ->
-                        if (dialogBinding.etNewName.text.toString().length > 9) {
-                            scrabblePlayer.playerName =
-                                dialogBinding.etNewName.text.toString().substring(0, 8)
-                        } else {
-                            scrabblePlayer.playerName = dialogBinding.etNewName.text.toString()
+                //edit player name button
+                ibChangeName.setOnClickListener {
+                    //create and show the dialog
+                    val dialogBinding = PopupPlayerNameBinding.inflate(layoutInflater)
+                    AlertDialog.Builder(context)
+                        .setView(dialogBinding.root)
+                        .setTitle(context.getString(R.string.change_player_name))
+                        .setPositiveButton(context.getString(R.string.change)) { _, _ ->
+                            if (dialogBinding.etNewName.text.toString().length > 9) {
+                                scrabblePlayer.playerName =
+                                    dialogBinding.etNewName.text.toString().substring(0, 8)
+                            } else {
+                                scrabblePlayer.playerName = dialogBinding.etNewName.text.toString()
+                            }
+                            this@ScrabbleAdapter.notifyItemChanged(adapterPosition)
                         }
-                        this@ScrabbleAdapter.notifyItemChanged(adapterPosition)
-                    }
-                    .setNegativeButton(context.getString(R.string.cancel), null)
-                    .setCancelable(false)
-                    .show()
+                        .setNegativeButton(context.getString(R.string.cancel), null)
+                        .setCancelable(false)
+                        .show()
+                }
             }
 
             //list of entered words
-            val listOfTextViews =
-                arrayListOf(binding.tv1, binding.tv2, binding.tv3, binding.tv4)
-            if (scrabblePlayer.listOfEnteredWords.size == 0) {
-                //if no word is entered, indicate it with a single textView
-                listOfTextViews[0].visibility = View.VISIBLE
-                listOfTextViews[0].text = context.getString(R.string.nothing_so_far)
-                //make rest of them gone
-                for (i in 1 until 4) {
-                    listOfTextViews[i].visibility = View.GONE
-                    listOfTextViews[i].text = ""
-                }
-            } else if (scrabblePlayer.listOfEnteredWords.size <= 4) {
-                //if there are less than 5 but higher than 0 entered words, show them
-                for (i in 0 until scrabblePlayer.listOfEnteredWords.size) {
-                    listOfTextViews[i].visibility = View.VISIBLE
-                    listOfTextViews[i].text = scrabblePlayer.listOfEnteredWords[i]
-                }
-                //but if they don't fill whole space, make rest of them gone
-                if (scrabblePlayer.listOfEnteredWords.size < 4) {
-                    for (i in scrabblePlayer.listOfEnteredWords.size until 4) {
-                        listOfTextViews[i].visibility = View.GONE
-                        listOfTextViews[i].text = ""
+            val listOfTextViews = arrayListOf(binding.tv1, binding.tv2, binding.tv3, binding.tv4)
+            when {
+                scrabblePlayer.listOfEnteredWords.isEmpty() -> {
+                    //if no word is entered, indicate it with a single textView
+                    listOfTextViews[0].apply {
+                        visibility = View.VISIBLE
+                        text = context.getString(R.string.nothing_so_far)
+                    }
+                    //and make rest of them gone
+                    listOfTextViews.subList(1, 4).forEach {
+                        it.visibility = View.GONE
+                        it.text = ""
                     }
                 }
-            } else {
-                //the only range that is left is where enteredWords > 4.
-                //in that case, show only two last entered words
-                for (i in listOfTextViews) {
-                    i.visibility = View.VISIBLE
+
+                scrabblePlayer.listOfEnteredWords.size <= 4 -> {
+                    //if there are less than 5 but higher than 0 entered words, show them
+                    scrabblePlayer.listOfEnteredWords.forEachIndexed { index, word ->
+                        listOfTextViews[index].apply {
+                            visibility = View.VISIBLE
+                            text = word
+                        }
+                    }
+                    //but if they don't fill whole space, make rest of them gone
+                    listOfTextViews.subList(scrabblePlayer.listOfEnteredWords.size, 4).forEach {
+                        it.visibility = View.GONE
+                        it.text = ""
+                    }
                 }
-                binding.tv1.text = "..."
-                binding.tv2.text =
-                    scrabblePlayer.listOfEnteredWords[scrabblePlayer.listOfEnteredWords.size - 3]
-                binding.tv3.text =
-                    scrabblePlayer.listOfEnteredWords[scrabblePlayer.listOfEnteredWords.size - 2]
-                binding.tv4.text = scrabblePlayer.listOfEnteredWords.last()
+
+                else -> {
+                    //the only range that is left is where enteredWords > 4.
+                    //in that case, show only two last entered words
+                    listOfTextViews.forEach { it.visibility = View.VISIBLE }
+                    binding.tv1.text = "..."
+                    binding.tv2.text =
+                        scrabblePlayer.listOfEnteredWords[scrabblePlayer.listOfEnteredWords.size - 3]
+                    binding.tv3.text =
+                        scrabblePlayer.listOfEnteredWords[scrabblePlayer.listOfEnteredWords.size - 2]
+                    binding.tv4.text = scrabblePlayer.listOfEnteredWords.last()
+                }
             }
 
             //button for showing list of entered words button
@@ -106,10 +112,10 @@ class ScrabbleAdapter(
             binding.tvScore.text = scrabblePlayer.totalPoints.toString()
 
             //color indicator
-            if (scrabblePlayer.myTurn) {
-                binding.cvPlayer.strokeColor = context.getColor(R.color.red)
+            binding.cvPlayer.strokeColor = if (scrabblePlayer.myTurn) {
+                context.getColor(R.color.red)
             } else {
-                binding.cvPlayer.strokeColor = context.getColor(R.color.white)
+                context.getColor(R.color.white)
             }
         }
     }
