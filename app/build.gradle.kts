a27f3f54ca86aec26f92b4da392135c138cee21a
Burakcan Duzcan <burakcanduzcan@gmail.com>
2025-07-14 17:39:54 +0300

    Increase target and compile SDK to 35 (Android 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = ("com.burakcanduzcan.tabletopcompanion")
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 4
         versionName = ("1.0.3")
 
