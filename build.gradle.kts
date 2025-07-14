d4bb8a9068acaf48d62c1857c98a783bc7ef028a
Burakcan Duzcan <burakcanduzcan@gmail.com>
2025-07-14 18:55:50 +0300

    Upgrade Hilt version to 2.56.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("com.android.application") version "8.8.2" apply false
     id("com.android.library") version "8.8.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
