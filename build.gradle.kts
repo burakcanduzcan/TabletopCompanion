1cd4d563a69788d85d98bc0c7e074e14e3c793e5
Burakcan Duzcan <burakcanduzcan@gmail.com>
2025-07-14 18:10:37 +0300

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.application") version "8.8.2" apply false
+    id("com.android.library") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
